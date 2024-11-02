4963fac455684406a49ed8d7d539eb092df94e3a
Abhishekjha20 <jha202001@gmail.com>
2024-11-02 13:05:54 +0530

    updated

---
+++ index.js
@@ -0,0 +1,24 @@
+// 4 step procedure to make server-->
+
+// Express ko bulana hoga es file me: 
+const express = require("express")
+
+// Express ko call karna padega ek variable me:
+const app = express();
+
+const dotenv = require("dotenv")
+// binding this env
+dotenv.config();
+
+// Port define karna hoga - Port hota hai darwaja:
+const port = process.env.PORT || 5000;
+
+// Making Routes:
+app.get("/", (req,res)=>{
+    res.send("Server runing.....")
+})
+
+// Server ko listen karna hoga:
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`)
+})
---
+++ notes.md
@@ -0,0 +1,1 @@
+# In which use  CommonJS Modules
