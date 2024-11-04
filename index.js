23121a20c026e8d12401f93860a5c9f4339b9f02
Abhishekjha20 <jha202001@gmail.com>
2024-11-04 09:55:48 +0530

    updated

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 // 4 step procedure to make server-->
 // Express ko bulana hoga es file me: 
-const express = require("express")
+const express = require("express");
 // Express ko call karna padega ek variable me:
 const app = express();
-const dotenv = require("dotenv")
+const dotenv = require("dotenv");
+const {readdirSync} = require("fs");
+
+
 // import the route here
-const authRoute = require("./routes/authRoutes")
+const authRoute = require("./routes/authRoutes");
 
 
 // binding this env
@@ -18,7 +21,15 @@ const port = process.env.PORT || 5000;
 app.get("/", (req,res)=>{
     res.send("Server runing.....")
 })
-app.use("/api", authRoute)
+// How to use routes
+// app.use("/api", authRoute)
+
+// importing and using routes dynamically____>>
+readdirSync("./routes").map((route)=>{
+ app.use("/api", require(`./routes/${route}`))
+})
+// console.log(readdirSync("./routes"))
+
 
 // Server ko listen karna hoga:
 app.listen(port,()=>{
